96b6b8aa90965c4483169c70841139bbfd219e05
nrmysarah <102278171+nrmysarah@users.noreply.github.com>
2022-05-20 11:14:20 +0800

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,15 @@
+int Led  =  5;
+ 
+void setup  ( )
+{
+  pinMode (Led, OUTPUT)  ; 
+}
+ 
+void loop  ( )  
+{
+  digitalWrite   (Led, HIGH)  ;  
+  delay (4000)  ; 
+  digitalWrite   (Led, LOW)  ;  
+  delay (1000)  ;  
+}
+
